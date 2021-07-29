f099a4111c50db0084cb2c9c0c0dfdeefad067dc
realaravinth <realaravinth@batsense.net>
2021-07-29 10:10:27 +0530

    improve malformed path error message

--- actix-router/src/resource.rs
+++ actix-router/src/resource.rs
@@ -932,7 +932,9 @@ impl ResourceDef {
                 }
                 _ => false,
             })
-            .expect("malformed dynamic segment");
+            .unwrap_or_else(|| {
+                panic!(r#"path "{}" contains malformed dynamic segment"#, pattern)
+            });
 
         let (mut param, mut unprocessed) = pattern.split_at(close_idx + 1);
 
