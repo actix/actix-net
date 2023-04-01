1681f86212c572932be00bed051db516a72f2455
Rob Ede <robjtede@icloud.com>
2023-04-01 18:15:24 +0100

    update syn to v2

--- actix-macros/tests/trybuild.rs
+++ actix-macros/tests/trybuild.rs
@@ -2,6 +2,7 @@
 #[test]
 fn compile_macros() {
     let t = trybuild::TestCases::new();
+
     t.pass("tests/trybuild/main-01-basic.rs");
     t.compile_fail("tests/trybuild/main-02-only-async.rs");
     t.pass("tests/trybuild/main-03-fn-params.rs");
