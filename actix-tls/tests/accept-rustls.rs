b6315a5d626e856cd52efcab8e579e3a10c6a062
dron <sarikarn120@gmail.com>
2026-09-13 18:07:51 +0530

    test(tls): verify handshake timeout on stalled connection in rustls

--- actix-tls/tests/accept-rustls.rs
+++ actix-tls/tests/accept-rustls.rs
@@ -1,27 +1,25 @@
-//! Use OpenSSL connector to test Rustls acceptor.
+//! Tests for Rustls acceptor.
 
-#![cfg(all(
-    feature = "accept",
-    feature = "connect",
-    feature = "rustls-0_23",
-    feature = "openssl"
-))]
+#![cfg(all(feature = "accept", feature = "rustls-0_23",))]
 
+#[cfg(feature = "openssl")]
 extern crate tls_openssl as openssl;
 
 use core::future::ready;
-use std::io::{BufReader, Write};
+use std::{io::BufReader, sync::mpsc, time::Duration};
+
+#[cfg(all(feature = "connect", feature = "openssl"))]
+use std::io::Write;
 
 use actix_rt::net::TcpStream;
 use actix_server::TestServer;
 use actix_service::ServiceFactoryExt as _;
-use actix_tls::{
-    accept::rustls_0_23::{reexports::ServerConfig, Acceptor, TlsStream},
-    connect::openssl::reexports::SslConnector,
+use actix_tls::accept::{
+    rustls_0_23::{reexports::ServerConfig, Acceptor, TlsStream},
+    TlsError,
 };
 use rustls_pemfile::{certs, pkcs8_private_keys};
 use rustls_pki_types_1::PrivateKeyDer;
-use tls_openssl::ssl::SslVerifyMode;
 
 fn new_cert_and_key() -> (String, String) {
     let rcgen::CertifiedKey { cert, signing_key } =
@@ -55,9 +53,17 @@ fn rustls_server_config(cert: String, key: String) -> ServerConfig {
     config
 }
 
-fn openssl_connector(cert: String, key: String) -> SslConnector {
-    use actix_tls::connect::openssl::reexports::SslMethod;
-    use openssl::{pkey::PKey, x509::X509};
+fn init_crypto() {
+    let _ = tokio_rustls_026::rustls::crypto::aws_lc_rs::default_provider().install_default();
+}
+
+#[cfg(all(feature = "connect", feature = "openssl"))]
+fn openssl_connector(
+    cert: String,
+    key: String,
+) -> actix_tls::connect::openssl::reexports::SslConnector {
+    use actix_tls::connect::openssl::reexports::{SslConnector, SslMethod};
+    use openssl::{pkey::PKey, ssl::SslVerifyMode, x509::X509};
 
     let cert = X509::from_pem(cert.as_bytes()).unwrap();
     let key = PKey::private_key_from_pem(key.as_bytes()).unwrap();
@@ -71,11 +77,10 @@ fn openssl_connector(cert: String, key: String) -> SslConnector {
     ssl.build()
 }
 
+#[cfg(all(feature = "connect", feature = "openssl"))]
 #[actix_rt::test]
 async fn accepts_connections() {
-    tokio_rustls_026::rustls::crypto::aws_lc_rs::default_provider()
-        .install_default()
-        .unwrap();
+    init_crypto();
 
     let (cert, key) = new_cert_and_key();
 
@@ -108,4 +113,40 @@ async fn accepts_connections() {
     stream.do_handshake().expect("TLS handshake failed");
 
     stream.flush().expect("TLS handshake failed");
+}
+
+#[actix_rt::test]
+async fn handshake_timeout() {
+    init_crypto();
+    let (cert, key) = new_cert_and_key();
+    let (tx, rx) = mpsc::channel();
+
+    let srv = TestServer::start({
+        let cert = cert.clone();
+        let key = key.clone();
+
+        move || {
+            let tx = tx.clone();
+            let mut tls_acceptor = Acceptor::new(rustls_server_config(cert.clone(), key.clone()));
+            tls_acceptor.set_handshake_timeout(Duration::from_millis(50));
+
+            tls_acceptor
+                .map_err(move |err| {
+                    let _ = tx.send(err);
+                })
+                .and_then(move |_stream: TlsStream<TcpStream>| ready(Ok(())))
+        }
+    });
+
+    let _sock = srv
+        .connect()
+        .expect("cannot connect to test server")
+        .into_std()
+        .unwrap();
+
+    let err = rx
+        .recv_timeout(Duration::from_secs(2))
+        .expect("server should emit timeout error for stalled handshake");
+
+    assert!(matches!(err, TlsError::Timeout));
 }
